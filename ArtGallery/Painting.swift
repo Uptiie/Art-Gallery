b6d929a9f3f958c42684adaa1820b67a5f012f31
Donovan Langager <donovan.lan16@gmail.com>
2019-06-05 13:46:11 -0700

    Setting up variables and functions to have the base layout of the app

---
+++ ArtGallery/Painting.swift
@@ -0,0 +1,17 @@
+//
+//  Painting.swift
+//  ArtGallery
+//
+//  Created by Uptiie on 6/5/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Painting {
+    required init() {
+        let image: UIImage
+        var isLiked = ""
+    }
+}
---
+++ ArtGallery/PaintingController.swift
@@ -0,0 +1,11 @@
+//
+//  PaintingController.swift
+//  ArtGallery
+//
+//  Created by Uptiie on 6/5/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+var painting: [Painting]
